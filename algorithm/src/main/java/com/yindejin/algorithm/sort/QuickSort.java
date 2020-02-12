184f926150808bc197603bc79f2b1f84a7977b1d
yindejin <yindejin@guazi.com>
2020-02-12 21:57:29 +0800

    快排

--- algorithm/src/main/java/com/yindejin/algorithm/sort/QuickSort.java
+++ algorithm/src/main/java/com/yindejin/algorithm/sort/QuickSort.java
@@ -8,8 +8,54 @@ package com.yindejin.algorithm.sort;
 public class QuickSort {
 
 
-    public void quickSort(int[] num) {
+    public void quickSort(int[] num, int left, int right) {
+        if (left > right) {
+            return;
+        }
 
+        int partition = partition(num, left, right);
+
+        quickSort(num, left, partition - 1);
+
+        quickSort(num, partition+1, right);
+    }
+
+
+    public int partition(int[] num, int left, int right){
+        if (left <= right) {
+            int temp = num[left];
+            int i = left, j = right;
+            while (i < j) {
+                while (num[j] >= temp && j > i){
+                    j--;
+                }
+                while (num[i] <= temp && j > i){
+                    i++;
+                }
+                if (i<j) {
+                    int tmp = num[i];
+                    num[i] = num[j];
+                    num[j] = tmp;
+                }
+            }
+            num[left] = num[i];
+            num[i] = temp;
+
+            return  i;
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {5,3,1,6,2};
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(nums, 0, nums.length - 1);
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
     }
 
 }
